303578a1903c6424a3823b78a841c8c810ebf38a
CyborgAB-3601 <102860184+CyborgAB-3601@users.noreply.github.com>
2023-06-22 20:24:55 +0530

    Add files via upload
    Change the path to where you download

---
+++ test2.py
@@ -0,0 +1,29 @@
+# Import the modules
+import ctypes
+import os
+import random
+import time
+
+# Define the path to the folder with images
+folder_path = "C:\\Users\\Abhinav\\3D Objects\\we2"
+
+# Get the list of image files in the folder
+image_files = [f for f in os.listdir(folder_path) if f.endswith(".jpg") or f.endswith(".png")]
+
+# Define a function to change the wallpaper
+def change_wallpaper(image_file):
+    # Get the full path to the image file
+    image_path = os.path.join(folder_path, image_file)
+    # Set the wallpaper using ctypes
+    ctypes.windll.user32.SystemParametersInfoW(20, 0, image_path, 0)
+    # Print a success message
+    print(f"Wallpaper changed to {image_file}!")
+
+# Loop indefinitely
+while True:
+    # Choose a random image file from the list
+    image_file = random.choice(image_files)
+    # Change the wallpaper using the function
+    change_wallpaper(image_file)
+    # Wait for one hour (10 seconds)
+    time.sleep(10)
